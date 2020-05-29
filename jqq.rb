3607bee31fdb0ae07e12025fd86cbdeb78d0b9fe
Jerry Chen <jerry@apache.org>
2020-05-29 07:10:16 -0500

    Cursor/readline-ish support. What a hot mess!

--- jqq.rb
+++ jqq.rb
@@ -9,10 +9,20 @@ FILE_Y = 0
 EXPR_Y = 1
 OUTPUT_Y = 2
 
+CSI_UP = 'A'
+CSI_DOWN = 'B'
+CSI_RIGHT = 'C'
+CSI_LEFT = 'D'
+
 KEY_BACKSPACE = 127
+KEY_CTRL_A = 1
 KEY_CTRL_D = 4
+KEY_CTRL_E = 5
+KEY_CTRL_K = 11
 KEY_CTRL_U = 21
 KEY_ENTER = 10
+KEY_ESCAPE = 27
+KEY_LEFT_BRACKET = '['
 KEY_WINDOW_RESIZE = 410
 
 def jq(args, opts={})
@@ -40,9 +50,10 @@ def print_title(title_win, file)
   title_win.refresh
 end
 
-def print_expr(expr_win, expr)
+def print_expr(expr_win, expr, expr_pos)
   expr_win.clear
   expr_win.addstr(expr)
+  expr_win.setpos(0, expr_pos)
   expr_win.refresh
 end
 
@@ -79,39 +90,103 @@ def curses_main(argv)
     0
   )
 
+  expr_pos = expr.size
+
   print_title(title_win, file)
-  print_expr(expr_win, expr)
+  print_expr(expr_win, expr, expr_pos)
   print_output(output_win, expr, file, :max_lines=>Curses.lines)
   expr_win.refresh
+
+  escape_mode = false
+  csi_mode = false # control sequence introducer
 
   running = true
   while running do
     render = false
+    should_echo = false
+
     begin
       key = expr_win.getch
 
-      case key
-      when KEY_BACKSPACE
-        expr = expr[0..-2]
-        print_expr(expr_win, expr)
-      when KEY_CTRL_D
-        running = false
-      when KEY_CTRL_U
-        expr = ""
-        print_expr(expr_win, expr)
-        expr_win.refresh
-      when KEY_ENTER
-        render = true
-      when KEY_WINDOW_RESIZE
-        render = true
+      if escape_mode
+        if csi_mode
+          case key
+          when CSI_UP
+            # TODO
+          when CSI_DOWN
+            # TODO
+          when CSI_LEFT
+            expr_pos = [0, expr_pos - 1].max
+            should_echo = true
+          when CSI_RIGHT
+            expr_pos = [expr.size, expr_pos + 1].min
+            should_echo = true
+          end
+
+          csi_mode = false
+          escape_mode = false
+        else # if not csi_mode
+          if key == KEY_LEFT_BRACKET
+            csi_mode = true
+          elsif key == 'b'
+            # alt-left
+            escape_mode = false
+          elsif key == 'f'
+            # alt-right
+            escape_mode = false
+          elsif key == 127
+            # alt-backspace
+            escape_mode = false
+          else
+            escape_mode = false
+          end
+        end
       else
-        expr += key.chr
-        print_expr(expr_win, expr)
+        case key
+        when KEY_BACKSPACE
+          if expr_pos > 0
+            # remove character at expr_pos, effectively
+            expr = expr[0...(expr_pos - 1)] + expr[expr_pos..-1]
+            expr_pos -= 1
+            should_echo = true
+          end
+        when KEY_CTRL_A
+          expr_pos = 0
+          should_echo = true
+        when KEY_CTRL_D
+          running = false
+        when KEY_CTRL_E
+          expr_pos = expr.size
+          should_echo = true
+        when KEY_CTRL_K
+          expr = expr[0...expr_pos]
+          expr_pos = expr.size
+          should_echo = true
+        when KEY_CTRL_U
+          expr = ""
+          expr_pos = 0
+          should_echo = true
+        when KEY_ENTER
+          render = true
+        when KEY_ESCAPE
+          escape_mode = true
+        when KEY_WINDOW_RESIZE
+          render = true
+        else
+          # add character at expr_pos
+          expr = expr[0...expr_pos] + key.chr + expr[expr_pos..-1]
+          expr_pos += 1
+          should_echo = true
+        end
       end
 
+      if should_echo
+        print_expr(expr_win, expr, expr_pos)
+        expr_win.refresh
+      end
       if render
         print_title(title_win, file)
-        print_expr(expr_win, expr)
+        print_expr(expr_win, expr, expr_pos)
         print_output(output_win, expr, file, :max_lines=>Curses.lines)
         expr_win.refresh
       end
